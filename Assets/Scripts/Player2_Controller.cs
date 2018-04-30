6f3c881a70898c2ae31776a75908ab5c22c03eac
sorararan <masolainota@gmail.com>
2018-04-30 18:28:54 +0900

    create_missile

--- Assets/Scripts/Player2_Controller.cs
+++ Assets/Scripts/Player2_Controller.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player2_Controller : MonoBehaviour {
 	Vector3 pos;
+	int count = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -12,6 +13,8 @@ public class Player2_Controller : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+		count++;
+
 		transform.localPosition = pos;
 
 		if (Input.GetKey (KeyCode.W)) {
@@ -19,6 +22,11 @@ public class Player2_Controller : MonoBehaviour {
 		}
 		if (Input.GetKey (KeyCode.S)) {
 			pos.y -= 0.2f;
+		}
+		if (Input.GetKey (KeyCode.D) && count >= 30) {
+			GameObject missile = (GameObject) Resources.Load ("Prefabs/missile2");
+			Instantiate (missile, pos, Quaternion.identity);
+			count = 0;
 		}
 
 		if (pos.y <= -5.5f || pos.y >= 5.5f) {
